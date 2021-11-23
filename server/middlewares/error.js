f6803ae46c85050c223c1426dfef23c5966d43e4
Dani Sanchez <danysq87@gmail.com>
2021-11-23 14:02:01 +0100

    Middleware error and notFoundErrorHandler and errorHandler test done

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -3,19 +3,19 @@ const { ValidationError } = require("express-validation");
 
 const debug = require("debug")("carers:server:error");
 
-const notFoundHandler = (req, res) => {
+const notFoundErrorHandler = (req, res) => {
   res.status(404).json({ error: "Page not found" });
 };
 
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (error, req, res, next) => {
+  debug(chalk.bgRed("An error has ocurred: "), chalk.bgRed(error.message));
   if (error instanceof ValidationError) {
     error.code = 400;
-    error.message = "Validation error";
+    error.message = "Bad request";
   }
-  debug(chalk.bgRed("An error has ocurred: "), chalk.bgRed(error.message));
   const message = error.code ? error.message : "General error!";
   res.status(error.code || 500).json({ error: message });
 };
 
-module.exports = { notFoundHandler, errorHandler };
+module.exports = { notFoundErrorHandler, errorHandler };
